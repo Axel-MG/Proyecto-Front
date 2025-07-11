d205b11b728da5f1ff5106d34e2844542855fdac
Axel-MG <axelwofor@gmail.com>
2025-07-10 21:06:39 -0600

    Creación de DetallePedido y PedidosCliente

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import CrearCliente from "./components/cliente/CrearCliente.js";
-import CrearProducto from "./components/Producto/CrearProducto.js";
-import PedidosCliente from "./components/pedido/PedidosCliente.js";
-import DetallePedido from "./components/pedido/DetallePedido.js";
 function App() {
   return (
     <div className="App">
@@ -13,9 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Bienvenido</h1>} />
         <Route path="/crear-cliente" element={<CrearCliente />} />
-        <Route path="/crear-producto" element={<CrearProducto />} />
-        <Route path="/pedidos-cliente/:id" element={<PedidosCliente />} />
-        <Route path="/detalle-pedido/:id" element={<DetallePedido />} />
       </Routes>
     </div>
   );
