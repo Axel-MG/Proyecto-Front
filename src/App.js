c91d57c7d09bfb9b1599d7ab26a8e8fcfe07d233
Axel-MG <axelwofor@gmail.com>
2025-07-10 21:06:39 -0600

    Creación de DetallePedido y PedidosCliente

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { Link, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import CrearCliente from "./components/cliente/CrearCliente.js";
+import PedidosCliente from "./components/pedido/PedidosCliente";
+import DetallePedido from "./components/pedido/DetallePedido";
+
+
+
 function App() {
   return (
     <div className="App">
@@ -30,6 +35,8 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Bienvenido</h1>} />
         <Route path="/crear-cliente" element={<CrearCliente />} />
+        <Route path="/pedidos-cliente/:id" element={<PedidosCliente />} />
+        <Route path="/detalle-pedido/:id" element={<DetallePedido />} />
       </Routes>
     </div>
   );
