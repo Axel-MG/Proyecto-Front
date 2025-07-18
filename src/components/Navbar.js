191972fd2e7cd7db89878f9ab4f0bf1b8a63dc19
Chilakiler73 <oscar.saldivar.sp@gmail.com>
2025-07-18 16:20:31 -0600

    slider de productos
    se agrego el slider de productos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { useContext, useState } from "react";
 import { ClienteContext } from "../context/ClienteContext";
 import { CarritoContext } from "../context/CarritoContext";
@@ -15,48 +15,49 @@ const Navbar = () => {
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Inicio
-        </Link>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/crear-cliente">
-                Crear Cliente
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/crear-producto">
-                Crear Producto
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/listar-productos">
-                Listar Productos
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="d-flex align-items-center">
-          <span className="navbar-text me-3">
-           {cliente
-             ? `Cliente Seleccionado: ${cliente.nombre}`
-             : "No hay cliente seleccionado"}
-         </span>
-          <Button variant="outline-light" onClick={handleMostrar} style={{ position: "relative" }}>
-              🛒 Carrito - ${total.toFixed(2)}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Inicio
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/crear-cliente">
+                  Crear Cliente
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/crear-producto">
+                  Crear Producto
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/listar-productos">
+                  Listar Productos
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="d-flex align-items-center">
+            <span className="navbar-text me-3">
+              {cliente
+                ? `Cliente Seleccionado: ${cliente.nombre}`
+                : "No hay cliente seleccionado"}
+            </span>
+            <Button variant="outline-light" onClick={handleMostrar} style={{ position: "relative" }}>
+               🛒 ${total.toFixed(2)}
               {carrito.length > 0 && (
                 <Badge bg="danger" pill style={{ position: "absolute", top: 0, right: 0 }}>
                   {carrito.reduce((acc, p) => acc + p.cantidad, 0)}
                 </Badge>
               )}
-          </Button>
+            </Button>
           </div>
         </div>
-    </nav>
-    {/* Modal carrito */}
+      </nav>
+
+      {/* Modal carrito */}
       <Modal show={showCarrito} onHide={handleCerrar} size="lg" centered>
         <Modal.Header closeButton>
           <Modal.Title>Carrito de Compras</Modal.Title>
@@ -65,7 +66,7 @@ const Navbar = () => {
           <CarritoModal />
         </Modal.Body>
       </Modal>
-  </>
+    </>
   );
 };
 
